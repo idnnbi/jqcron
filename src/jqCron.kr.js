2e49862e8ce089d1934936ac7407c444fff970a7
dnnbi <idnnbi@gmail.com>
2016-05-04 16:27:44 +0900

    Create jqCron.kr.js

---
+++ src/jqCron.kr.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the Arnapou jqCron package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+jqCronDefaultSettings.texts.kr = {
+    empty: '( 주기 선택 )',
+    empty_minutes: '분 선택',
+    empty_time_hours: '시 선택',
+    empty_time_minutes: '분 선택',
+    empty_day_of_week: '요일 선택',
+    empty_day_of_month: '일 선택',
+    empty_month: '월 선택',
+    name_minute: '분',
+    name_hour: '( 시간 마다 )',
+    name_day: '( 일  마다 )',
+    name_week: '( 주  마다 )',
+    name_month: '( 월  마다 )',
+    name_year: '( 년  마다 )',
+    text_period: '주기 :  <b />',
+    text_mins: ' - <b /> 분에 실행',
+    text_time: ' - <b /> 시 :<b /> 분에 실행',
+    text_dow: ' <b />',
+    text_month: '일 <b />',
+    text_dom: ' <b />',
+    error1: '태그 %s 는 지원되지 않습니다!',
+    error2: '잘못된 값입니다',
+    error3: 'jqeury_element는 jqCron에서 셋팅 되어야 합니다.',
+    error4: '잘못된 양식 입니다.',
+    weekdays: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+    months: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월']
+};
